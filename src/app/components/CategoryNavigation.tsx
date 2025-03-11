0992e15a8c0e049e67c51c518e970aa1792c3159
Vincentius <vincentiussss@gmail.com>
2025-03-11 23:18:55 +0700

    mockup product categories list

---
+++ src/app/components/CategoryNavigation.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+const CategoryNavigation = () => {
+  return (
+    <div className="flex mx-6 gap-5 text-lg overflow-x-scroll flex-nowrap">
+      <div className="hover:border-b border-black">aio</div>
+      <div>aio</div>
+      <div>aio</div>
+      <div>aio</div>
+      <div>aio</div>
+      <div>aio</div>
+      <div>aio</div>
+      <div>aio</div>
+      <div>aio</div>
+      <div>aio22</div>
+      <div>aio22</div>
+      <div>aio22</div>
+      <div>aio22</div>
+    </div>
+  );
+};
+
+export default CategoryNavigation;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Header from "./components/Header";
-import MobileWrapper from "./components/MobileWrapper";
+import Header from "../components/Header";
+import MobileWrapper from "../components/MobileWrapper";
+import CategoryNavigation from "./components/CategoryNavigation";
 
 export default function Home() {
   return (
     <MobileWrapper>
       <Header />
+      <CategoryNavigation />
     </MobileWrapper>
   );
 }
