01b91317c8c7ee0f55c2a49e31b77e0584501162
Vincentius <vincentiussss@gmail.com>
2025-03-14 19:07:55 +0700

    cart page

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,12 @@
+import MobileWrapper from "@/components/MobileWrapper";
+import CartDisplay from "./components/CartDisplay";
+
+const CartPage = () => {
+  return (
+    <MobileWrapper>
+      <CartDisplay />
+    </MobileWrapper>
+  );
+};
+
+export default CartPage;
