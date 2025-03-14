175790ae27eff30d8aa464b9b1e543db52c4ac3d
Vincentius <vincentiussss@gmail.com>
2025-03-14 10:23:20 +0700

    modify returned cart as hashmap

--- src/services/cartService.tsx
+++ src/services/cartService.tsx
@@ -1,13 +1,33 @@
 import { API_BASE_URL } from "@/constants/api";
-import { ProductCartMap } from "@/types/cart";
+import { CartItem, ProductCartMap } from "@/types/cart";
 import axios from "axios";
 
-export const getCart = async (): Promise<ProductCartMap[]> => {
+export const getCart = async (): Promise<ProductCartMap> => {
   try {
     const response = await axios.get(`${API_BASE_URL}/cart`);
-    return response.data;
+    
+    const productCartMap: ProductCartMap = response.data.reduce(
+      (acc: ProductCartMap, item: CartItem) => {
+        acc[item.productId] = item;
+        return acc;
+      },
+      {} as ProductCartMap
+    );
+
+    return productCartMap; 
   } catch (error) {
     console.log("Error fetching products data, error: " + error);
+    throw error;
+  }
+};
+
+
+export const addToCart = async (data: CartItem) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/cart`, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error submitting item to cart:", error);
     throw error;
   }
 };
