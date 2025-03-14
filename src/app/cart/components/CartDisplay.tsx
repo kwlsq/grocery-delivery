67a16e0ee86a3ed2a2e6e8da96ec4f2b52c797c8
Vincentius <vincentiussss@gmail.com>
2025-03-14 23:57:03 +0700

    cart display

--- src/app/cart/components/CartDisplay.tsx
+++ src/app/cart/components/CartDisplay.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { useCartContext } from "@/context/cartContext";
 import Image from "next/image";
 
 const CartDisplay = () => {
+  const { cartItems } = useCartContext();
+  console.log(cartItems);
   return (
     <div>
       <div className="flex flex-col px-6 gap-4 py-2 mt-6">
@@ -13,47 +16,56 @@ const CartDisplay = () => {
           src="/icons/ic-close.svg"
           className="h-3.5 w-3.5 "
         />
+
         <h1 className="text-4xl font-bold">Cart</h1>
       </div>
 
-      {/* kita kasih mapping */}
-      <div className="flex gap-5 items-end px-6">
-        <Image
-          height={100}
-          width={100}
-          alt="icon close cart"
-          src="/products/garlic.webp"
-          className="h-24"
-        />
-        <div className="flex justify-center flex-col gap-2">
-          <h2 className="font-semibold text-[17px]">Garlic</h2>
-          <div className="flex items-center gap-10">
-            {/* create custom comp add,substract product*/}
-            <div className="flex items-center gap-5">
-              <button className=" bg-grey rounded-full w-10 h-10 flex justify-center items-center">
-                <Image
-                  height={100}
-                  width={100}
-                  alt="icon substract product"
-                  src="/icons/ic-substract.svg"
-                  className="w-3"
-                />
-              </button>
-              <div>1kg</div>
-              <button className=" bg-grey rounded-full w-10 h-10 flex justify-center items-center">
-                <Image
-                  height={100}
-                  width={100}
-                  alt="icon add product"
-                  src="/icons/ic-add.svg"
-                  className="w-6"
-                />
-              </button>
+      {cartItems?.map((cartItem) => {
+        return (
+          <div key={cartItem.id} className="flex gap-5 items-center px-6 h-24 my-3">
+            <Image
+              height={100}
+              width={100}
+              alt="icon close cart"
+              src={cartItem.imageUrl}
+              className="h-full"
+            />
+
+            <div className="flex justify-center flex-col gap-2">
+              <h2 className="font-semibold text-[17px]">{cartItem.name}</h2>
+              <div className="flex items-center w-200">
+                {/* create custom comp add,substract product*/}
+                <div className="flex items-center ">
+                  <button className=" bg-grey rounded-full w-10 h-10 flex justify-center items-center">
+                    <Image
+                      height={100}
+                      width={100}
+                      alt="icon substract product"
+                      src="/icons/ic-substract.svg"
+                      className="w-3"
+                    />
+                  </button>
+                  <div className="w-20">
+                    {(cartItem.weight / 1000).toFixed(1)}kg
+                  </div>
+                  <button className=" bg-grey rounded-full w-10 h-10 flex justify-center items-center">
+                    <Image
+                      height={100}
+                      width={100}
+                      alt="icon add product"
+                      src="/icons/ic-add.svg"
+                      className="w-6"
+                    />
+                  </button>
+                </div>
+                <div className="opacity-55 ">
+                  {`$` + (cartItem.price * 1000).toFixed(1)}
+                </div>
+              </div>
             </div>
-            <div className="opacity-55">$5.1</div>
           </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 };
--- src/context/cartContext.tsx
+++ src/context/cartContext.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-import { getCart, addToCart, updateCartItem, removeFromCart } from "@/services/cartService";
+import {
+  getCart,
+  addToCart,
+  updateCartItem,
+  removeFromCart,
+} from "@/services/cartService";
 import { ProductCartMap, CartItem } from "@/types/cart";
 import {
   createContext,
@@ -10,12 +15,19 @@ import {
   useEffect,
   useState,
 } from "react";
+import { useProductContext } from "./productContext";
+import { Product } from "@/types/product";
 
 interface CartContextType {
   cart: ProductCartMap | undefined;
+  cartItems: Product[] | undefined;
   addToCart: (data: CartItem) => Promise<void>;
-  updateCartItem: (productId: number, updatedData: Partial<CartItem>) => Promise<void>;
+  updateCartItem: (
+    productId: number,
+    updatedData: Partial<CartItem>
+  ) => Promise<void>;
   removeFromCart: (productId: number) => Promise<void>;
+  getCartItemsFromCartId: () => void;
 }
 
 const CartContext = createContext<CartContextType | undefined>(undefined);
@@ -29,39 +41,61 @@ export const useCartContext = () => {
 };
 
 export const CartProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const { productList } = useProductContext();
   const [cart, setCart] = useState<ProductCartMap | undefined>();
+  const [cartItems, setCartItems] = useState<Product[] | undefined>();
 
   const updateCart = async (): Promise<void> => {
     const cartItems = await getCart();
     setCart(cartItems);
   };
 
-  useEffect(() => {
-    updateCart();
-  }, []);
-
   const handleAddToCart = async (data: CartItem) => {
     await addToCart(data);
-    updateCart(); 
+    updateCart();
   };
-  
-  const handleUpdateCartItem = async (productId: number, updatedData: Partial<CartItem>) => {
+
+  const handleUpdateCartItem = async (
+    productId: number,
+    updatedData: Partial<CartItem>
+  ) => {
     await updateCartItem(productId, updatedData);
-    updateCart(); 
+    updateCart();
   };
 
   const handleRemoveFromCart = async (productId: number) => {
     await removeFromCart(productId);
-    updateCart(); 
+    updateCart();
   };
+
+  const getCartItemsFromCartId = () => {
+    const productMap = new Map(productList?.map((p) => [p.id, p]));
+    const cartItems = Object.values(cart ?? {})
+      .map((item) => productMap.get(item.id))
+      .filter((product): product is Product => product !== undefined);
+
+    setCartItems(cartItems);
+  };
+
+  useEffect(() => {
+    updateCart();
+  }, []);
+
+  useEffect(() => {
+    if (productList && cart) {
+      getCartItemsFromCartId();
+    }
+  }, [productList, cart]);
 
   return (
     <CartContext.Provider
       value={{
         cart,
+        cartItems,
         addToCart: handleAddToCart,
         updateCartItem: handleUpdateCartItem,
         removeFromCart: handleRemoveFromCart,
+        getCartItemsFromCartId,
       }}
     >
       {children}
